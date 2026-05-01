0434afc1a9a591686f0591177bdbc0a5e639959b
Jaiane <jaianesoares700@gmail.com>
2026-05-01 19:04:36 -0300

    feat: first commit

---
+++ src/main/java/com/sopro/project_demoday/ProjectDemodayApplication.java
@@ -0,0 +1,13 @@
+package com.sopro.project_demoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDemodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDemodayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sopro/project_demoday/ProjectDemodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopro.project_demoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
