69844d68c6e25b84debbd75abbfaa3d000ce6d72
Jaiane <jaianesoares700@gmail.com>
2026-05-01 19:32:19 -0300

    feat: feito WebConfig e variaveis de ambiente

--- src/main/java/com/sopro/project_demoday/config/WebConfig.java
+++ src/main/java/com/sopro/project_demoday/config/WebConfig.java
@@ -1,4 +1,16 @@
 package com.sopro.project_demoday.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class WebConfig {
-}
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                    .allowedOrigins("https://seu-projeto-sopro.vercel.app") // URL falsa por enquanto
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        }
+    }
+
