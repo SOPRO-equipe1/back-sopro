9296853c2aa6e4bd35494d8dfa011832af64c669
Jaiane <jaianesoares700@gmail.com>
2026-06-15 09:17:15 -0300

    fix: ajustado UsuarioCadastroDTO para aumentar segurança de senha e email no login

--- src/main/java/com/sopro/project_demoday/dto/UsuarioCadastroDTO.java
+++ src/main/java/com/sopro/project_demoday/dto/UsuarioCadastroDTO.java
@@ -2,62 +2,67 @@ package com.sopro.project_demoday.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import com.sopro.project_demoday.model.enums.RoleUsuario;
 
 public class UsuarioCadastroDTO {
-    
-    @NotBlank(message = "Nome é obrigatório")
+
+    @NotBlank(message = "O nome é obrigatório")
     private String nome;
-    
-    @Email(message = "Email deve ser válido")
-    @NotBlank(message = "Email é obrigatório")
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Insira um formato de e-mail válido (exemplo@dominio.com)")
     private String email;
-    
-    @NotBlank(message = "Senha é obrigatória")
-    @Size(min = 8, message = "Senha deve ter pelo menos 8 caracteres")
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!_\\-*&%#@])(?=\\S+$).{8,}$",
+            message = "A senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial"
+    )
     private String senha;
-    
+
     private RoleUsuario role;
-    
+
     public UsuarioCadastroDTO() {
     }
-    
+
     public UsuarioCadastroDTO(String nome, String email, String senha, RoleUsuario role) {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
         this.role = role;
     }
-    
+
     public String getNome() {
         return nome;
     }
-    
+
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
+
     public String getEmail() {
         return email;
     }
-    
+
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
     public String getSenha() {
         return senha;
     }
-    
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
-    
+
     public RoleUsuario getRole() {
         return role;
     }
-    
+
     public void setRole(RoleUsuario role) {
         this.role = role;
     }
